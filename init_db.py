8e689dc995477d41fae7b8c09ec1ae96a1f58601
Martin Booth <mrtnbth@gmail.com>
2024-01-12 14:39:03 +0200

    Initial commit

---
+++ init_db.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from db import get_db
+
+app = Flask(__name__)
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+init_db()
---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE products (
+  id INT PRIMARY KEY,
+  sku TEXT UNIQUE,
+  attributes TEXT
+)
